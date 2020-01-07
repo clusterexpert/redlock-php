d60e6bd1f1b6eb659e59f93657850f99f78804ad
Max Mercuriev <max@mercuriev.dev>
2020-01-07 20:19:30 +0700

    throw exception if lock aquiring failed

--- src/RedLock.php
+++ src/RedLock.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/LockAcquiringTimeoutException.php';
 
 class RedLock
 {
@@ -67,7 +68,7 @@ class RedLock
 
         } while ($retry > 0);
 
-        return false;
+        throw new LockAcquiringTimeoutException();
     }
 
     public function unlock(array $lock)
